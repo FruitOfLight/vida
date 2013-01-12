0a909f1e4ed678acef9c7f2c5705f3ed6192cce7
jablkoj <jablko.jahoda@gmail.com>
2013-01-12 14:12:03 +0100

    Revert "idcko sa hovori programom"
    This reverts commit b456194cadb66434b1bf4bf215202195bfa77f3b.

--- src/Graph.java
+++ src/Graph.java
@@ -42,6 +42,8 @@ public class Graph implements Drawable {
     }
 
     public void draw(Graphics g) {
+    	g.setColor(new Color(255, 255, 255));
+        g.fillRect(0, 0, width,		height);
         g.setColor(new Color(0, 0, 0));
         g.drawRect(0, 0, width-1, height-1);
         // vykresli polhranu
