06dcde3fa87c9fe88cc356f968bf569e642c45ac
jablkoj <jablko.jahoda@gmail.com>
2013-01-12 14:12:03 +0100

    Revert "idcko sa hovori programom"
    This reverts commit b456194cadb66434b1bf4bf215202195bfa77f3b.

--- src/MessageQueue.java
+++ src/MessageQueue.java
@@ -62,6 +62,8 @@ public class MessageQueue implements Drawable {
 
     public void draw(Graphics g) {
         // TODO
+    	g.setColor(new Color(255, 255, 255));
+    	g.fillRect(0, 0, width, height);
     	g.setColor(new Color(0, 0, 0));
     	g.drawRect(0, 0, width-1, height-1);
     }
